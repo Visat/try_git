b28c147acc3525d5adb8b7d1731ecd1d8e606a3d
Visat <citydreamer17@gmail.com>
2015-04-10 10:58:18 +0800

    hellogit.c 2nd commit.

--- hellogit.c
+++ hellogit.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+//#define SWITCH 1
+
 int main(int argc, char* argv[])
 {
     printf("Hello Git!\n");
+
+
+#ifdef SWITCH
+
+    printf("\nSwitch On.\n");
+
+#endif
    
     return 0;
 }
